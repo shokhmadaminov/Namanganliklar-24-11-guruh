b60a84dcffa1c5cfb1024693f50a986659039418
shokhmadaminov <shokhmadaminov@gmail.com>
2022-05-12 18:01:10 +0500

    add notification section

---
+++ app.js
@@ -0,0 +1,20 @@
+const notificationBtn = document.getElementById("notification-btn")
+const modalBg = document.getElementById("modal-bg")
+const modalClose = document.querySelector(".modal-cancel")
+
+
+
+notificationBtn.addEventListener("click", function() {
+    modalBg.classList.remove("hidden")
+})
+
+modalClose.addEventListener("click", function() {
+    modalBg.classList.add("hidden")
+})
+
+document.addEventListener("keydown", function(e) {
+    if(e.keyCode == 27) {
+        modalBg.classList.add("hidden")
+    }
+})
+
